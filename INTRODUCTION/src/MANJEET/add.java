f5fcb52008538c5d000b5003b63cae8e0bc66a1e
Manjeet-Ram <manjitram888@gmail.com>
2024-02-13 22:30:38 +0530

    commit-msg

---
+++ INTRODUCTION/src/MANJEET/add.java
@@ -0,0 +1,9 @@
+package MANJEET;
+
+public class add {
+ public static void main(String[] x) {
+	 int a = Integer.parseInt(x[0]);
+	 int b = Integer.parseInt(x[1]);
+	 System.out.println(+(a+b));
+ }
+}
